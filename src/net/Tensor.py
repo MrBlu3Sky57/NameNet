cb98f21b5df6c4a94cf14fb2d74bc8e27e6c89f7
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2025-06-10 17:49:01 -0400

    Tensor class done

---
+++ src/net/Tensor.py
@@ -0,0 +1,31 @@
+"""
+File containing the Tensor Class
+"""
+
+
+import numpy as np
+
+class Tensor():
+    """
+    Tensor class that stores value and grad arrays meant to represent
+    a matrix or vector in a neural network
+    """
+    value: np.ndarray
+    grad: np.ndarray
+
+    def __init__(self, value: np.ndarray):
+        self.value = value
+        self.grad = np.zeros(value.shape)
+    
+    def zero_grad(self):
+        """
+        Set the gradients to zero
+        """
+        self.grad = np.zeros(self.value.shape)
+    def increment(self, lr):
+        """
+        Increment the value based on the gradient with the given learning
+        rate
+        """
+        self.value -= lr * self.grad
+
