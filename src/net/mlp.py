69a754d2df92b0a7f9f7eca51cb2810cd4f70b00
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2025-06-11 02:19:28 -0400

    Backprop done

--- src/net/mlp.py
+++ src/net/mlp.py
@@ -8,7 +8,7 @@ from net import Tensor
 class MLP():
     """
     Multilayer perceptron class that supports forward and backward pass
-    operations
+    operations using cross entropy loss and soft max output
     """
     layers: list[Tensor]
     unact: list[Tensor]
@@ -42,26 +42,27 @@ class MLP():
         self.layers[0].value = xs
         for i in range(1, len(self.layers)):
             if len(xs.shape) == 1:
-                self.biases.value = np.reshape(self.biases[i].value, shape=len(self.biases[i].value))
-                self.unact[i].value = self.layers[i - 1].value @ self.weights[i].value.T + self.biases[i].value
-            else:
-                self.biases.value = np.reshape(self.biases[i].value, shape=(1, len(self.biases[i].value)))
+                self.biases[i].value = np.reshape(self.biases[i].value, shape=len(self.biases[i].value))
                 self.unact[i].value = self.layers[i - 1].value @ self.weights[i].value.T + self.biases[i].value
                 if i != len(self.layers) - 1:
                     self.layers[i].value = self.sigma(self.unact[i].value)
-        self.layers[-1].value = self._soft_max(self.layers[-1].value)
+            else:
+                self.biases[i].value = np.reshape(self.biases[i].value, shape=(1, len(self.biases[i].value)))
+                self.unact[i].value = self.layers[i - 1].value @ self.weights[i].value.T + self.biases[i].value
+                if i != len(self.layers) - 1:
+                    self.layers[i].value = self.sigma(self.unact[i].value)
+        self.layers[-1].value = self._soft_max(self.unact[-1].value)
 
-    def backward(self, dl: callable):
+    def backward(self, y_onehot):
         """
         Run a backward pass on the network (Assuming network gets matrix inputs)
         """
-        self.layers[-1].grad = dl(self.layers[-1].value)
-        self.unact[-1].grad = np.sum(self._deriv_soft_max(self.unact[-1].value) * self.layers[-1].grad, axis=0, keepdims=True)
+        self.unact[-1].grad = self.layers[-1].value - y_onehot
         for i in range(1, len(self.layers)):
-            self.weights[-i].grad = np.sum(self.layers[-(i+1)].value, axis=0, keepdims=True).T * self.unact[-(i+1)].grad
-            self.biases[-i].grad = (self.unact[-1].grad).flatten()
-            self.layers[-(i + 1)].grad = np.sum(self.weights[-(i + 1)].T * self.unact[-1].grad, axis=1, keepdims=True).T
-            self.unact[-(i + 1)].grad = self.dsigma(self.layers[-i].grad[-(i + 1)])
+            self.weights[-i].grad = self.layers[-(i+1)].value.T @ self.unact[-i].grad
+            self.biases[-i].grad = np.sum(self.unact[-i].grad, axis=0)
+            self.layers[-(i + 1)].grad = self.unact[-i].grad @ self.weights[-i].value
+            self.unact[-(i + 1)].grad = self.layers[-(i+1)].grad * self.dsigma(self.unact[-(i + 1)].value)
         return
 
     @staticmethod
@@ -80,10 +81,3 @@ class MLP():
             x = x - np.max(x, axis=1) # Stability
             logits = np.exp(x)
             return logits / np.sum(logits, axis=1)
-    
-    @staticmethod
-    def _deriv_soft_max(x: np.ndarray):
-        """
-        Softmax derivative
-        """
-        return x * (1 - x)
