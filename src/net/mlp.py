c6a7d5ac7d80ee05f207590186ef5d3c5570eb04
Aaron <61670051+MrBlu3Sky57@users.noreply.github.com>
2025-06-10 23:26:25 -0400

    softmax

--- src/net/mlp.py
+++ src/net/mlp.py
@@ -30,7 +30,7 @@ class MLP():
             self.layers.append(Tensor(None))
             self.unact.append(Tensor(None))
             self.weights.append(Tensor(np.random.randn(l2, l1)))
-            self.biases.append(Tensor(np.random.randn(1, l2)))
+            self.biases.append(Tensor(np.random.randn(1, l2))) # Could lead to a bug!!
     
     def forward(self, xs: np.ndarray):
         """
@@ -42,11 +42,25 @@ class MLP():
             self.unact[i].value = self.layers[i - 1].value @ self.weights[i].value.T + self.biases[i].value
             self.layers[i].value = self.sigma(self.unact[i].value)
     
-    def backward(self, dL: callable):
-        """Run a backward pass on the network"""
+    def backward(self, dl: callable):
+        """
+        Run a backward pass on the network
+        """
         pass
     
     def _soft_max(self, x: np.ndarray):
-        """ """
-        pass
+        """ 
+        If the array is of dimension 1 apply elementwise soft max
+        otherwise apply row wise.
+        """
+        if len(x.shape) == 0:
+            raise ValueError
+        if len(x.shape) == 1:
+            x = x - np.max(x) # Stability
+            logits = np.exp(x)
+            return logits / np.sum(logits)
+        if len(x.shape) > 1:
+            x = x - np.max(x, axis=1) # Stability
+            logits = np.exp(x)
+            return logits / np.sum(logits, axis=1)
         
